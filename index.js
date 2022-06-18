cc96bbdfd332b73c48f288f67e5ec00211e0283d
root <root@HomeLPT-001.localdomain>
2022-06-18 22:08:20 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const alphanumericOnly = word.toLowerCase()
+    .match(/[a-z0-9]/g)
+  return alphanumericOnly.join('') ===
+    alphanumericOnly.reverse().join('');
 }
-
-/* 
-  Add your pseudocode here
-*/
 
 /*
   Add written explanation of your solution here
+  Lowercase input via word.toLowerCase();
+Match all alphanumeric characters using a regular expression via word.match(/[a-z0-9]/g)...
+this match returns an array of found words
+Use Array.reverse and Array.join on the alphanumeric matches to 
+compare the original against its reversed self. 
+If they're identical we get back true, otherwise we get back false!
 */
 
 // You can run `node index.js` to view these console logs
